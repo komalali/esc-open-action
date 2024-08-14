15bead6172fdfa54296d911e492bb5cecfdebeee
Komal <komal@pulumi.com>
2024-08-13 23:19:54 -0700

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as esc from '@pulumi/esc-sdk'
 
 /**
  * The main function for the action.
@@ -7,18 +7,30 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const config = new esc.Configuration({
+      accessToken: process.env.PULUMI_ACCESS_TOKEN
+    })
+    const client = new esc.EscApi(config)
+    const fqen = core.getInput('environment')
+    const [orgName, environmentName] = fqen.split('/')
+    console.log(`Opening environment ${environmentName}!`)
+    const openEnvironmentResponse = await client.openEnvironment(
+      orgName,
+      environmentName
+    )
+    console.log(`Opened environment ${environmentName}!`)
+    if (!openEnvironmentResponse?.id) {
+      throw new Error(`Failed to open environment ${environmentName}`)
+    }
+    console.log('Reading open environment')
+    const readEnvironmentResponse = await client.readOpenEnvironment(
+      orgName,
+      environmentName,
+      openEnvironmentResponse?.id
+    )
+    console.log(readEnvironmentResponse)
+    const time = new Date().toTimeString()
+    core.setOutput('time', time)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
